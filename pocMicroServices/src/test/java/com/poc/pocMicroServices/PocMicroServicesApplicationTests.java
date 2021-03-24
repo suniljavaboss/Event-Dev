305d5fd7fb3116da4202e8b3ff7159e27d72f3bf
suniljboss <suniljavaboss@gmail.com>
2021-03-25 03:11:48 +0530

    First commit

---
+++ pocMicroServices/src/test/java/com/poc/pocMicroServices/PocMicroServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poc.pocMicroServices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocMicroServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
